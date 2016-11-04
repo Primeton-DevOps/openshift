e34a2ed7086efc72e702e0bbced7633b6dec2889
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-04 13:57:21 +0800

    Update

---
+++ src/test/java/com/primeton/devops/openshift/resource/QuotaApiTestCase.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.primeton.devops.openshift.resource;
+
+import com.primeton.devops.openshift.testcase.AbstractTestCase;
+
+/**
+ * @author ZhongWen (mailto:lizhongwen1989@gmail.com)
+ *
+ */
+public class QuotaApiTestCase extends AbstractTestCase {
+
+	/* (non-Javadoc)
+	 * @see com.primeton.devops.openshift.testcase.AbstractTestCase#test()
+	 */
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
