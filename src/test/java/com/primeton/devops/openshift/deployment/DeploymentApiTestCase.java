146b70ee6ac6792b82a2e752f425ecdc4c0343e2
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-04 12:48:08 +0800

    Update

--- src/test/java/com/primeton/devops/openshift/deployment/DeploymentApiTestCase.java
+++ src/test/java/com/primeton/devops/openshift/deployment/DeploymentApiTestCase.java
@@ -9,6 +9,8 @@ import com.openshift.restclient.ResourceKind;
 import com.openshift.restclient.images.DockerImageURI;
 import com.openshift.restclient.model.IContainer;
 import com.openshift.restclient.model.IDeploymentConfig;
+import com.openshift.restclient.model.IService;
+import com.openshift.restclient.model.route.IRoute;
 import com.primeton.devops.openshift.testcase.AbstractTestCase;
 import com.primeton.devops.openshift.util.OpenshiftClient;
 
@@ -21,6 +23,9 @@ public class DeploymentApiTestCase extends AbstractTestCase {
 	private String deploymentConfigName = "cfg-nginx-" + uid;
 	private String containerName = "c-nginx-" + uid;
 	private String labelName = "label-nginx-" + uid;
+	private String serviceName = "s-nginx-" + uid;
+	private String domainName = "nginx" + uid + ".openshift.primeton.com";
+	private String routeName = "r-nginx-" + uid;
 
 	/* (non-Javadoc)
 	 * @see com.primeton.devops.openshift.testcase.AbstractTestCase#test()
@@ -36,17 +41,41 @@ public class DeploymentApiTestCase extends AbstractTestCase {
 		IContainer container = deploymentConfig.addContainer(containerName);
 		container.setImage(new DockerImageURI("nginx:latest"));
 		
-		deploymentConfig.setReplicas(2); // container size
+		deploymentConfig.setReplicas(2); // pod size
+		//deploymentConfig.setDesiredReplicaCount(2); // pod size
 		
 		deploymentConfig.addLabel("name", labelName);
 		
 		deploymentConfig.setReplicaSelector("name", labelName);
 		
+		// create pod
 		IDeploymentConfig dc = OpenshiftClient.getClient().create(deploymentConfig);
 		Assert.assertNotNull(dc);
 		
 		System.out.println("DeploymentConfig '" + deploymentConfigName + "' success created. Will delete after 600 seconds.");
+		
+		// create service
+		IService service = OpenshiftClient.getClient().getResourceFactory().stub(ResourceKind.SERVICE, serviceName, projectName);
+		service.setSelector("name", labelName); // bind target pod
+		service.addPort(80, 80); // port : targetPort (container)
+		service = OpenshiftClient.getClient().create(service);
+		Assert.assertNotNull(service);
+		System.out.println(String.format("Create service %s success.", service));
+		
+		// create route
+		IRoute route = OpenshiftClient.getClient().getResourceFactory().stub(ResourceKind.ROUTE, routeName, projectName);
+		route.setHost(domainName);
+		// route.setPath("/");
+		route.setServiceName(serviceName); // binding target service
+		route = OpenshiftClient.getClient().create(route);
+		Assert.assertNotNull(route);
+		System.out.println(String.format("Create route %s for service '%s' success.", route, serviceName));
+		
 		sleep(600);
+		
+		// clean resources
+		OpenshiftClient.getClient().delete(route);
+		OpenshiftClient.getClient().delete(service);
 		OpenshiftClient.getClient().delete(dc);
 	}
 
