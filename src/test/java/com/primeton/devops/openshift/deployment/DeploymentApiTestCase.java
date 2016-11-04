e881f51f9c3c38a6818a3cc2175938430a8a3d2a
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-04 11:52:02 +0800

    Update

--- src/test/java/com/primeton/devops/openshift/deployment/DeploymentApiTestCase.java
+++ src/test/java/com/primeton/devops/openshift/deployment/DeploymentApiTestCase.java
@@ -3,20 +3,51 @@
  */
 package com.primeton.devops.openshift.deployment;
 
+import org.junit.Assert;
+
+import com.openshift.restclient.ResourceKind;
+import com.openshift.restclient.images.DockerImageURI;
+import com.openshift.restclient.model.IContainer;
+import com.openshift.restclient.model.IDeploymentConfig;
 import com.primeton.devops.openshift.testcase.AbstractTestCase;
+import com.primeton.devops.openshift.util.OpenshiftClient;
 
 /**
  * @author ZhongWen (mailto:lizhongwen1989@gmail.com)
  *
  */
 public class DeploymentApiTestCase extends AbstractTestCase {
+	
+	private String deploymentConfigName = "cfg-nginx-" + uid;
+	private String containerName = "c-nginx-" + uid;
+	private String labelName = "label-nginx-" + uid;
 
 	/* (non-Javadoc)
 	 * @see com.primeton.devops.openshift.testcase.AbstractTestCase#test()
 	 */
 	@Override
 	public void test() {
+		IDeploymentConfig deploymentConfig = OpenshiftClient.getClient().getResourceFactory()
+				.stub(ResourceKind.DEPLOYMENT_CONFIG, deploymentConfigName, projectName);
 		
+		deploymentConfig.setEnvironmentVariable("MEM_MAX", "2048");
+		deploymentConfig.setEnvironmentVariable("MEM_MIN", "1024");
+		
+		IContainer container = deploymentConfig.addContainer(containerName);
+		container.setImage(new DockerImageURI("nginx:latest"));
+		
+		deploymentConfig.setReplicas(2); // container size
+		
+		deploymentConfig.addLabel("name", labelName);
+		
+		deploymentConfig.setReplicaSelector("name", labelName);
+		
+		IDeploymentConfig dc = OpenshiftClient.getClient().create(deploymentConfig);
+		Assert.assertNotNull(dc);
+		
+		System.out.println("DeploymentConfig '" + deploymentConfigName + "' success created. Will delete after 600 seconds.");
+		sleep(600);
+		OpenshiftClient.getClient().delete(dc);
 	}
 
 }
