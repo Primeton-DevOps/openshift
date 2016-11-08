a57ac1bcb90eec3e96422c56f63cf5efe1dddbe4
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-08 14:35:00 +0800

    Update

--- src/test/java/com/primeton/devops/openshift/project/ProjectManageApiTestCase.java
+++ src/test/java/com/primeton/devops/openshift/project/ProjectManageApiTestCase.java
@@ -4,6 +4,7 @@
 package com.primeton.devops.openshift.project;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -12,7 +13,7 @@ import org.junit.Test;
 import com.openshift.internal.restclient.model.Project;
 import com.openshift.restclient.ResourceKind;
 import com.openshift.restclient.model.IProject;
-import com.openshift.restclient.model.IResource;
+import com.openshift.restclient.model.project.IProjectRequest;
 import com.primeton.devops.openshift.util.OpenshiftClient;
 
 /**
@@ -24,17 +25,17 @@ public class ProjectManageApiTestCase {
 	
 	@Test
 	public void test() throws Exception {
-		final String projectName = "myfirstproject";
-		// 1
-		IResource request = OpenshiftClient.getClient().getResourceFactory().stub(ResourceKind.PROJECT_REQUEST, projectName);
-		// 2
+		final String projectName = UUID.randomUUID().toString().replaceAll("-", "");
+		// way 1
+		IProjectRequest request = OpenshiftClient.getClient().getResourceFactory().stub(ResourceKind.PROJECT_REQUEST, projectName);
+		// way 2
 		/*
 		IProject request = OpenshiftClient.getClient().getResourceFactory().stub(ResourceKind.PROJECT, projectName);
+		*/
 		request.setDescription("This is my first project.");
 		request.setDisplayName("My First Project");
-		*/
 		IProject project = (IProject)OpenshiftClient.getClient().create(request);
-		System.out.println(String.format("Project '%s' success created.", project));
+		System.out.println(String.format("Project '%s' success created.", projectName));
 
 		Assert.assertNotNull(project);
 		
@@ -43,19 +44,21 @@ public class ProjectManageApiTestCase {
 		project = OpenshiftClient.getClient().get(ResourceKind.PROJECT, projectName, null);
 		Assert.assertNotNull(project);
 		
-		project.setDescription("This is my first project.");
-		project.setDisplayName("My First Project");
+		project.setDescription("This is my second project.");
+		project.setDisplayName("My Second Project");
 		
-		OpenshiftClient.getClient().update(project);
+		project = OpenshiftClient.getClient().update(project);
+		System.out.println(String.format("Project '%s' success updated.", projectName));
 		
 		List<Project> projects = OpenshiftClient.getClient().list(ResourceKind.PROJECT);
 		Assert.assertNotNull(projects);
 		Assert.assertFalse(projects.isEmpty());
 		
-		System.out.println("Project '" + projectName + "' success created. Will auto delete after 30 seconds.");
+		System.out.println(String.format("Project '%s' will auto delete after 30 seconds.", projectName));
 		TimeUnit.SECONDS.sleep(30);
 		
 		OpenshiftClient.getClient().delete(project);
+		System.out.println(String.format("Project '%s' success deleted.", projectName));
 	}
 
 }
