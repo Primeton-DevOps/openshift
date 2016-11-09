f45c36e834adebd851fcfe33e652b771292c67ff
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-09 09:28:42 +0800

    Update

--- src/main/java/com/primeton/devops/openshift/util/YamlJsonUtil.java
+++ src/main/java/com/primeton/devops/openshift/util/YamlJsonUtil.java
@@ -4,8 +4,10 @@
 package com.primeton.devops.openshift.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.yaml.snakeyaml.Yaml;
 
@@ -20,6 +22,11 @@ public class YamlJsonUtil {
 	
 	private YamlJsonUtil(){}
 	
+	/**
+	 * 
+	 * @param json
+	 * @return
+	 */
 	@SuppressWarnings("rawtypes")
 	public static String json2yaml(String json) {
 		if (StringUtils.isBlank(json)) {
@@ -35,6 +42,27 @@ public class YamlJsonUtil {
 		return null == obj ? null : new Yaml().dump(obj);
 	}
 	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static String json2yaml(InputStream input) {
+		try {
+			return null == input ? null : json2yaml(IOUtils.toString(input));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(input);
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param yaml
+	 * @return
+	 */
 	@SuppressWarnings("rawtypes")
 	public static String yaml2json(String yaml) {
 		if (StringUtils.isBlank(yaml)) {
@@ -42,9 +70,25 @@ public class YamlJsonUtil {
 		}
 		Map obj = (Map)new Yaml().load(yaml);
 		try {
-			return new ObjectMapper().writeValueAsString(obj);
+			return null == obj ? null : new ObjectMapper().writeValueAsString(obj);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static String yaml2json(InputStream input) {
+		try {
+			return null == input ? null : yaml2json(IOUtils.toString(input));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(input);
 		}
 		return null;
 	}
